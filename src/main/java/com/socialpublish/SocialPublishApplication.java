42f71fdcf3d345340fdd40d5bdefab7bc696cf51
polchduikt <polchilliakney@gmail.com>
2026-05-01 12:47:00 +0300

    Initial project structure

---
+++ src/main/java/com/socialpublish/SocialPublishApplication.java
@@ -0,0 +1,13 @@
+package com.socialpublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPublishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialPublishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socialpublish/SocialPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialpublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPublishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
